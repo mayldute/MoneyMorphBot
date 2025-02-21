7b037d758ffd7e0a3e63073fbabaa283cf9f0e98
aliaksandra <126246067+mayldute@users.noreply.github.com>
2025-02-21 18:39:44 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,14 +7,14 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message: telebot.types.Message):
     """Handle /start and /help commands"""
-    text = f'Чтобы начать работу введите комманду боту в следующем формате:\n<имя вылюты>\
-<в какyю валюту перевести>\
-<количество валюты>\nУвидить все доступные валюты: /values'
+    text = f'To start working, enter the command to the bot in the following format:\n<currency name>\
+<currency to convert to>\
+<amount of currency>\nTo see all available currencies: /values'
     bot.send_message(message.chat.id, text)
     
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
-    text = 'Доступные валюты:'
+    text = 'Available currencies:'
     
     for key in keys.keys():
         text = '\n'.join((text, key, ))
@@ -28,16 +28,16 @@ def convert(message: telebot.types.Message):
         values = message.text.split(' ')
         
         if len(values) != 3:
-            raise APIException('Некорректное количество параметров. Повторите попытку ввода.')
+            raise APIException('Incorrect number of parameters. Please try again.')
             
         quote, base, amount = values
         total_base = CurrencyConverter.get_price(quote, base, amount)
     except APIException as e:
-        bot.send_message(message.chat.id, f'Ошибка пользователя.\n{e}')
+        bot.send_message(message.chat.id, f'User error.\n{e}')
     except Exception as e:
-        bot.send_message(message.chat.id, f'Не удалось обработать команду. \n{e}')
+        bot.send_message(message.chat.id, f'Failed to process the command. \n{e}')
     else:
-        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        text = f'The price of {amount} {quote} in {base} is {total_base}'
         bot.send_message(message.chat.id, text)
     
 bot.polling()
