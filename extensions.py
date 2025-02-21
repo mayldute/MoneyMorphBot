07a8ed08f9e0d73b99bf059855be7c0a592f7736
aliaksandra <126246067+mayldute@users.noreply.github.com>
2025-02-21 18:38:27 +0100

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -10,22 +10,22 @@ class CurrencyConverter:
     def get_price(quote: str, base: str, amount: str):
         
         if quote == base:
-            raise APIException(f'Невозможно перевести одинаковые валюты {quote}.')
+            raise APIException(f'Unable to convert the same currencies {quote}.')
         
         try:
             quote_ticker = keys[quote]
         except KeyError:
-            raise APIException(f'Не удалось обработать валюту {quote}!')
+            raise APIException(f'Failed to process currency {quote}!')
         
         try:
             base_ticker = keys[base]
         except KeyError:
-            raise APIException(f'Не удалось обработать валюту {base}!')
+            raise APIException(f'Failed to process currency {base}!')
         
         try:
             amount = float(amount)
         except ValueError:
-            raise APIException(f'Не удалось обработать количество {amount}!')
+            raise APIException(f'Failed to process amount {amount}!')
         
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = json.loads(r.content)[keys[base]] * float(amount)
